bf8e004d2b599e36fc1cd0f408cb1a1350ad8b25
Davide Lancierini <davide@farm-ui.physik.uzh.ch>
2019-02-26 15:17:30 +0100

    Added plot function

--- utils.py
+++ utils.py
@@ -3,7 +3,38 @@ import pickle
 import numpy as np
 import sys
 
+import matplotlib.pyplot as plt
+
 def sel_eff(tp,threshold):
     
     sig_eps = np.float(np.where(tp>threshold)[0].shape[0])/np.float(tp.shape[0])
     return sig_eps
+
+def plot_loss_acc(model_history, validation=None):
+    
+    if validation:
+    
+        plt.subplot(1,2,1)
+        plt.plot(model_history.history['val_loss'])
+        plt.xlabel('Epoch')
+        plt.title('Loss on train set')
+        plt.subplot(1,2,2)
+        plt.plot(model_history.history['val_acc'])
+        plt.xlabel('Epoch')
+        plt.title('Accuracy on train set')
+        fig = plt.gcf()
+        fig.set_size_inches(15,5)
+        
+    else:
+        
+        plt.subplot(1,2,1)
+        plt.plot(model_history.history['loss'])
+        plt.xlabel('Epoch')
+        plt.title('Loss on train set')
+        plt.subplot(1,2,2)
+        plt.plot(model_history.history['acc'])
+        plt.xlabel('Epoch')
+        plt.title('Accuracy on train set')
+        fig = plt.gcf()
+        fig.set_size_inches(15,5)
+       
